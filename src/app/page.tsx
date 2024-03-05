c93b2ee57c9bb9107bdba24c96e09d00d9c54de8
dev-yun <yunpro2766@gmail.com>
2024-03-06 00:38:58 +0900

    feat: storybook test를 위한 CreateAccount component 생성
    - shadcn의 input, label, card 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button";
+import { CreateAccount } from "../components/test/CreateAccount";
 
 export default function Home() {
   return (
-    <main className="lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+    <main className="flex flex-col items-center justify-center lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 space-y-16">
       <div className="flex flex-col items-center justify-center w-full h-full space-y-4">
         <h1 className="text-4xl font-bold">Hello World</h1>
         <Button>Click me</Button>
+      </div>
+      <div>
+        <CreateAccount />
       </div>
     </main>
   );
