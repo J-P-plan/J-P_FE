6efb46db6d19c96088864e98278941bc0901bf6d
dgd03146 <ibory1220@naver.com>
2024-03-09 15:21:39 +0000

    chore: set up i18n

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,12 +3,18 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 import React from "react";
-import { cn } from "../lib/utils";
+import { cn } from "../../lib/utils";
+import { dir } from "i18next";
+import i18nConfig from "../../../i18nConfig";
 
 const notoSans = Noto_Sans_KR({
   subsets: ["latin"],
   weight: ["100", "400", "700", "900"],
 });
+
+export function generateStaticParams() {
+  return i18nConfig.locales.map(locale => ({ locale }));
+}
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,11 +23,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  params: { locale },
 }: Readonly<{
   children: React.ReactNode;
+  params: { locale: string };
 }>) {
   return (
-    <html lang="en">
+    <html lang={locale} dir={dir(locale)}>
       <body className={cn("min-h-screen", notoSans.className)}>{children}</body>
     </html>
   );
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,41 @@
+import { Button } from "../../components/ui/button";
+import { CreateAccount } from "../../components/test/CreateAccount";
+import ExampleClientComponent from "../../components/test/ExampleClientComponent";
+import LanguageChanger from "../../components/test/LanguageChanger";
+import TranslationProvider from "../../components/test/TranslationsProvider";
+import initTranslations from "../i18n";
+
+// import LanguageChanger from "../../components/test/LanguageChanger";
+
+type Params = {
+  params: {
+    locale: string;
+  };
+};
+
+const i18nNamespaces = ["home"];
+
+export default async function Home({ params: { locale } }: Params) {
+  const { t, resources } = await initTranslations(locale, i18nNamespaces);
+
+  return (
+    <main className="flex flex-col items-center justify-center lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 space-y-16">
+      <div className="flex flex-col items-center justify-center w-full h-full space-y-4">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+        <Button>Click me</Button>
+      </div>
+      <div>
+        <CreateAccount />
+      </div>
+      <h1>{t("header")}</h1>
+      <TranslationProvider
+        locale={locale}
+        resources={resources}
+        namespaces={i18nNamespaces}
+      >
+        <ExampleClientComponent />
+        <LanguageChanger />
+      </TranslationProvider>
+    </main>
+  );
+}
