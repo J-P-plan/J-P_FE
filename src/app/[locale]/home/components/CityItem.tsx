e026d181aac907703e69e1964ca708c3b4ad1b2c
dev-yun <yunpro2766@gmail.com>
2024-04-10 16:50:17 +0900

    feat: home page에서 여행지, 도시, 테마별 컴포넌트 구현
    - / page에서 home으로 redirect 시키는 코드 추가(locale 때문에 / page를 home으로 하지 않음

---
+++ src/app/[locale]/home/components/CityItem.tsx
@@ -0,0 +1,27 @@
+import { IPlace } from "@/lib/types/place";
+import Image from "next/image";
+import React from "react";
+
+interface Props {
+  place: IPlace;
+}
+
+const CityItem = ({ place }: Props) => {
+  const img = "https://www.ghibli.jp/gallery/ponyo016.jpg";
+  return (
+    <li className="flex-none flex items-center gap-4 relative">
+      <Image
+        src={img}
+        alt="travel"
+        className="w-28 h-28 object-cover rounded-lg"
+        width={120}
+        height={120}
+      />
+      <p className="absolute right-4 bottom-3 text-white font-['SUIT'] font-bold leading-[normal] capitalize">
+        {place.name}
+      </p>
+    </li>
+  );
+};
+
+export default CityItem;
