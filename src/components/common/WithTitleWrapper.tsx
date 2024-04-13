f78550bad128c4cecf027f67a22798dffa7ee791
dev-yun <yunpro2766@gmail.com>
2024-04-13 14:23:35 +0900

    refactor: PlaceList component refactor, main에서 공통으로 사용될 title Wrapper component 추가

---
+++ src/components/common/WithTitleWrapper.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface Props {
+  title: string;
+  children: React.ReactNode;
+}
+
+const WithTitleWrapper = ({ title, children }: Props) => {
+  return (
+    <div className="flex flex-col overflow-x-auto gap-3">
+      <p className="text-[#1a1a1a] font-['SUIT'] text-xl font-bold leading-[normal] capitalize">
+        {title}
+      </p>
+      {children}
+    </div>
+  );
+};
+
+export default WithTitleWrapper;
