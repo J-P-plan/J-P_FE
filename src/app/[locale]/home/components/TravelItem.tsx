20ec62e82c4ec124b997a961c2a033a66b5e10d7
dev-yun <yunpro2766@gmail.com>
2024-04-14 14:26:07 +0900

    feat: 화면에서 미리 보이는 next/image에 priority 속성 추가

--- src/app/[locale]/home/components/TravelItem.tsx
+++ src/app/[locale]/home/components/TravelItem.tsx
@@ -16,6 +16,7 @@ const TravelItem = ({ place }: Props) => {
         className="w-30 h-30 object-cover rounded-lg"
         width={120}
         height={120}
+        priority
       />
       <div className="flex flex-col gap-1">
         <p className="text-sm font-bold leading-[normal] capitalize">
