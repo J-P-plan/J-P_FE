9d27532c9a1b938bb6af86aedcf89af55b3d9048
dev-yun <yunpro2766@gmail.com>
2024-04-23 00:12:14 +0900

    fix: 첫 렌더링 시 화면에 보이는 이미지만 preload하도록 코드 수정

--- src/app/[locale]/home/components/ThemeItem.tsx
+++ src/app/[locale]/home/components/ThemeItem.tsx
@@ -4,9 +4,10 @@ import React from "react";
 
 interface Props {
   place: IPlace;
+  priority?: boolean;
 }
 
-const ThemeItem = ({ place }: Props) => {
+const ThemeItem = ({ place, priority }: Props) => {
   const img = "https://www.ghibli.jp/gallery/ponyo016.jpg";
   return (
     <li className="flex-none flex items-center relative">
@@ -16,7 +17,7 @@ const ThemeItem = ({ place }: Props) => {
         className="w-36 h-32 object-cover rounded-lg"
         width={144}
         height={128}
-        priority
+        priority={priority}
       />
       <div className="absolute left-4 bottom-3 text-white">
         <p className="title flex-shrink-0 w-[4.4375rem] h-[1.0625rem] overflow-hidden text-white whitespace-no-wrap text-sm font-bold leading-[normal] capitalize">
--- src/app/[locale]/home/components/TravelItem.tsx
+++ src/app/[locale]/home/components/TravelItem.tsx
@@ -4,9 +4,10 @@ import React from "react";
 
 interface Props {
   place: IPlace;
+  priority?: boolean;
 }
 
-const TravelItem = ({ place }: Props) => {
+const TravelItem = ({ place, priority }: Props) => {
   const img = "https://www.ghibli.jp/gallery/ponyo016.jpg";
   return (
     <li className="flex-none flex items-center flex-col gap-2">
@@ -16,7 +17,7 @@ const TravelItem = ({ place }: Props) => {
         className="w-30 h-30 object-cover rounded-lg"
         width={120}
         height={120}
-        priority
+        priority={priority}
       />
       <div className="flex flex-col gap-1">
         <p className="text-sm font-bold leading-[normal] capitalize">
