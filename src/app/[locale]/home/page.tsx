fb83cdd57e4115fbfb504332342c0f0c917fc404
신윤철 <71367408+dev-yun@users.noreply.github.com>
2024-04-24 22:00:18 +0900

    Revert "feat: searchPlace 검색 component 추가"
    This reverts commit 6721015b95ed4cb7da08e89cd356ed0708d2f2aa.

--- src/app/[locale]/home/page.tsx
+++ src/app/[locale]/home/page.tsx
@@ -3,15 +3,12 @@ import MobileMenu from "@/components/common/MobileMenu";
 import PlaceList from "./components/PlaceList";
 import React from "react";
 import ReviewList from "./components/ReviewList";
-import SearchPlace from "./components/SearchPlace";
 import TravelStoryList from "./components/TravelStoryList";
 
 const page = async () => {
   return (
     <div className="flex flex-col p-4 gap-6">
       <MobileHomeHeader />
-
-      <SearchPlace />
 
       <PlaceList placeType="TRAVEL" moreHref="travels" />
       <PlaceList placeType="CITY" moreHref="cities" />
