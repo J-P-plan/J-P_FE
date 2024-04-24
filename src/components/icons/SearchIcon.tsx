e4b409cb5efe428aee98e8c065b1831caf6c0e92
신윤철 <71367408+dev-yun@users.noreply.github.com>
2024-04-24 22:00:18 +0900

    Revert "feat: mobile bottom navigation 구현"
    This reverts commit a85ade75020cb020509bcc40c7cc0af9a3e2ae05.

--- src/components/icons/SearchIcon.tsx
+++ src/components/icons/SearchIcon.tsx
@@ -4,14 +4,12 @@ interface Props {
   className?: string;
   fill?: string;
   stroke?: string;
-  strokeWidth?: string;
 }
 
 const SearchIcon = ({
   className,
   fill = "none",
-  stroke = "#4D4D4D",
-  strokeWidth = "1.5",
+  stroke = "#1A1A1A",
 }: Props) => {
   return (
     <svg
@@ -25,7 +23,7 @@ const SearchIcon = ({
       <path
         d="M16.9269 17.04L20.4 20.4M19.28 11.44C19.28 15.7699 15.7699 19.28 11.44 19.28C7.11009 19.28 3.60001 15.7699 3.60001 11.44C3.60001 7.11006 7.11009 3.59998 11.44 3.59998C15.7699 3.59998 19.28 7.11006 19.28 11.44Z"
         stroke={stroke}
-        strokeWidth={strokeWidth}
+        strokeWidth="1.5"
         strokeLinecap="round"
       />
     </svg>
