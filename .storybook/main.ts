ebdf3e6941f8360d25890d4db0738950ab627191
dev-yun <yunpro2766@gmail.com>
2024-04-25 21:48:16 +0900

    fix: build 에러 수정, chromatic action file 제거....
    - 계속 Failed to build the preview SB_BUILDER-WEBPACK5_0002 (WebpackInvocationError) 에러 발생..

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,7 +11,11 @@ const config: StorybookConfig = {
   ],
   framework: {
     name: "@storybook/nextjs",
-    options: {},
+    options: {
+      builder: {
+        useSWC: true,
+      },
+    },
   },
   docs: {
     autodocs: "tag",
