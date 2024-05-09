b4a2b75c9e2388d05ff4d850ed5dc70c29ebc2f2
dev-yun <yunpro2766@gmail.com>
2024-05-09 23:20:53 +0900

    fix: change root page component name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Home() {
+export default function page() {
   return (
     <main className="flex flex-col items-center justify-center lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 space-y-16">
       메인
